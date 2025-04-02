0a9bff6404fa5e1ebbe7d9867605ba406e5e7002
Sofiia <sofiia.kuhicvhak.pz.2023@lpnu.ua>
2025-04-02 19:53:56 +0300

    task06

---
+++ task06/dnapp/lambdas/audit_producer/Function.cs
@@ -0,0 +1,104 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.DynamoDBEvents;
+using System;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+namespace SimpleLambdaFunction;
+
+
+public class Function
+{
+	private static readonly string AuditTableName = Environment.GetEnvironmentVariable("table_name") ?? "Audit";
+	private readonly AmazonDynamoDBClient _dynamoDbClient = new AmazonDynamoDBClient();
+
+	public async Task FunctionHandler(DynamoDBEvent dynamoEvent, ILambdaContext context)
+	{
+		foreach (var record in dynamoEvent.Records)
+		{
+			if (record.EventName == "INSERT")
+			{
+				await HandleInsert(ConvertAttributeValues(record.Dynamodb.NewImage));
+			}
+			else if (record.EventName == "MODIFY")
+			{
+				await HandleModify(ConvertAttributeValues(record.Dynamodb.OldImage), ConvertAttributeValues(record.Dynamodb.NewImage));
+			}
+		}
+	}
+
+	private async Task HandleInsert(Dictionary<string, AttributeValue> newImage)
+	{
+		var auditItem = new Dictionary<string, AttributeValue>
+		{
+			["id"] = new AttributeValue { S = Guid.NewGuid().ToString() },
+			["itemKey"] = new AttributeValue { S = newImage["key"].S },
+			["modificationTime"] = new AttributeValue { S = DateTime.UtcNow.ToString("o") },
+			["newValue"] = new AttributeValue
+			{
+				S = JsonSerializer.Serialize(new
+				{
+					key = newImage["key"].S,
+					value = int.Parse(newImage["value"].N)
+				})
+			}
+		};
+
+		await _dynamoDbClient.PutItemAsync(AuditTableName, auditItem);
+	}
+
+	private async Task HandleModify(Dictionary<string, AttributeValue> oldImage, Dictionary<string, AttributeValue> newImage)
+	{
+		string updatedAttribute = "value";
+		int oldValue = int.Parse(oldImage["value"].N);
+		int newValue = int.Parse(newImage["value"].N);
+
+		var auditItem = new Dictionary<string, AttributeValue>
+		{
+			["id"] = new AttributeValue { S = Guid.NewGuid().ToString() },
+			["itemKey"] = new AttributeValue { S = newImage["key"].S },
+			["modificationTime"] = new AttributeValue { S = DateTime.UtcNow.ToString("o") },
+			["updatedAttribute"] = new AttributeValue { S = updatedAttribute },
+			["oldValue"] = new AttributeValue { N = oldValue.ToString() },
+			["newValue"] = new AttributeValue { N = newValue.ToString() }
+		};
+
+		await _dynamoDbClient.PutItemAsync(AuditTableName, auditItem);
+	}
+
+	private Dictionary<string, Amazon.DynamoDBv2.Model.AttributeValue> ConvertAttributeValues(Dictionary<string, Amazon.Lambda.DynamoDBEvents.DynamoDBEvent.AttributeValue> originalImage)
+	{
+		var convertedImage = new Dictionary<string, Amazon.DynamoDBv2.Model.AttributeValue>();
+
+		foreach (var item in originalImage)
+		{
+			var attributeValue = item.Value;
+			var newAttributeValue = new Amazon.DynamoDBv2.Model.AttributeValue();
+
+			// Depending on the attribute type, map it accordingly
+			if (attributeValue.S != null)
+			{
+				newAttributeValue.S = attributeValue.S;
+			}
+			else if (attributeValue.N != null)
+			{
+				newAttributeValue.N = attributeValue.N;
+			}
+			else if (attributeValue.B != null)
+			{
+				newAttributeValue.B = attributeValue.B;
+			}
+			// Add more conditions for other data types if needed
+
+			convertedImage[item.Key] = newAttributeValue;
+		}
+
+		return convertedImage;
+	}
+}

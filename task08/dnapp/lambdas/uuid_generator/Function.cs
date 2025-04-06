fb6f3dc727793774dacf04a3ff87b0b81440baeb
Sofiia <sofiia.kuhicvhak.pz.2023@lpnu.ua>
2025-04-06 21:52:13 +0300

    task08

--- task08/dnapp/lambdas/uuid_generator/Function.cs
+++ task08/dnapp/lambdas/uuid_generator/Function.cs
@@ -2,18 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Amazon.Lambda.Core;
-using Amazon.Lambda.S3Events;
 using Amazon.S3;
 using Amazon.S3.Model;
 using Newtonsoft.Json;
+using Amazon.Lambda.APIGatewayEvents;  // Required for API Gateway events
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace SimpleLambdaFunction;
-
-public class Function
+namespace SimpleLambdaFunction
 {
-	public class FunctionHandler
+	public class Function
 	{
 		private readonly AmazonS3Client _s3Client;
 		private readonly string _bucketName;
@@ -22,10 +20,11 @@ public class Function
 		{
 			_s3Client = new AmazonS3Client();
 			// Retrieve the S3 bucket name from environment variables
-			_bucketName = Environment.GetEnvironmentVariable("target_bucket");
+			_bucketName = Environment.GetEnvironmentVariable("target_bucket") ?? throw new InvalidOperationException("Target bucket environment variable is not set.");
 		}
 
-		public async Task FunctionHandler(LambdaEvent input, ILambdaContext context)
+		// Replace LambdaEvent with the correct event type, in this case, APIGatewayProxyRequest
+		public async Task FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
 		{
 			// Generate 10 random UUIDs
 			var uuidList = new List<string>();
@@ -47,7 +46,7 @@ public class Function
 			var jsonContent = JsonConvert.SerializeObject(content);
 
 			// Create the PutObject request
-			var putObjectRequest = new PutObjectRequest
+			var putObjectRequest = new Amazon.S3.Model.PutObjectRequest
 			{
 				BucketName = _bucketName,  // Use the bucket name from environment variable
 				Key = fileName,  // File name is the current time
